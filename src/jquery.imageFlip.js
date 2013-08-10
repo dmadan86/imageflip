d737d5dc210d1ec8246ff24687f842d34854f260
Madan <dmadan86@gmail.com>
2013-08-10 21:31:47 +0530

    Updated the code with test page

--- src/jquery.imageFlip.js
+++ src/jquery.imageFlip.js
@@ -16,45 +16,45 @@
         verticalFlip: true
     };
 
+    function imageFlip(container, options ){
+        if(!container.is("img")) return;                            //A general check whether the object is an Image
 
-    function imageFlip(container, config ){
-        if(!container.is("img")) return;                        //A general check whether the object is an Image
-
-        config = $.extend($.fn.imageFlip.defaults ,config );    //Override the existing settings
-
+        var config = $.extend($.fn.imageFlip.defaults ,options );    //Override the existing settings
         var canvas = $("<canvas/>"),
             ctx = canvas.get(0).getContext("2d");
 
+        var flipH = config.horizontalFlip,
+            flipV = config.verticalFlip,
+            scaleH = flipH ? -1 : 1,                            // Set horizontal scale to -1 if flip horizontal
+            scaleV = flipV ? -1 : 1,                            // Set verical scale to -1 if flip vertical
+            posX, posY;
+
         //Wait for the image to get Loaded
-        container.bind("load", function(){
-            loaded(config.horizontalFlip, config.verticalFlip)
-        });
+        if(config.canvas && support.canvasSupport){
+            container[0].onload = function(){
+                renderCanvas();
+            };
 
-        function loaded(flipH, flipV, isRendered){
-            var scaleH = flipH ? -1 : 1,                        // Set horizontal scale to -1 if flip horizontal
-                scaleV = flipV ? -1 : 1,                        // Set verical scale to -1 if flip vertical
-                posX = flipH ? container.width() * -1 : 0,      // Set x position to -100% if flip horizontal
-                posY = flipV ? container.height() * -1 : 0;     // Set y position to -100% if flip vertical
-
-            if(config.canvas && support.canvasSupport){
-                if(!isRendered){
-                    container.hide().wrap("<div style='position:relative' />");
-                    var obj = {
-                        width: container.width(),
-                        height: container.height()
-                    };
-                    canvas.attr(obj).css(obj).appendTo(container.parent());
-                }
-
-                ctx.save();                                     // Save the current state
-                ctx.scale(scaleH, scaleV);                      // Set scale to flip the image
-                ctx.drawImage(container.get(0), posX, posY);    // draw the image
-                ctx.restore();                                  // Restore the last saved state
-
-            }else{
-                container.css(support.transform, "scale(" + scaleH + "," + scaleV + ")");
-            }
+        }else{
+            container.css(support.transform, "scale(" + scaleH + "," + scaleV + ")");
         }
+
+        var renderCanvas = function (){
+            console.log(flipH)
+            container.hide().wrap("<div style='position:relative' />");
+            var obj = {
+                width: container.width(),
+                height: container.height()
+            };
+
+            canvas.attr(obj).css(obj).appendTo(container.parent());
+            posX = flipH ? container.width() * -1 : 0;      // Set x position to -100% if flip horizontal
+            posY = flipV ? container.height() * -1 : 0;     // Set y position to -100% if flip vertical
+            ctx.save();                                     // Save the current state
+            ctx.scale(scaleH, scaleV);                      // Set scale to flip the image
+            ctx.drawImage(container.get(0), posX, posY);    // draw the image
+            ctx.restore();                                  // Restore the last saved state
+        };
 
         this.setFlip = function(horizontalFlip, verticalFlip){
             loaded(horizontalFlip, verticalFlip, true)
@@ -73,6 +73,4 @@
     support = {};
     support.transform = getVendorPropertyName('transform');
     support.canvasSupport = !!window.HTMLCanvasElement;
-
-
 })(document, window, undefined, jQuery);
